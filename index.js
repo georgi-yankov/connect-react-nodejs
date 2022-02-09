92315822d291264baefa8095346aeb79874a4525
Georgi Yankov <georgi.st.yankov@gmail.com>
2022-02-09 22:54:53 +0200

    use .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const dotenv = require("dotenv").config();
 const express = require("express")
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 const cors = require("cors")
 
 app.use(express.urlencoded({ extended: true }))
